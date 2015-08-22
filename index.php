fc962210d8a606569886dbd9e8012f9979a9f9bf
fflesch <fflesch.dev@gmail.com>
2015-08-22 17:42:57 +0200

    étoffage de l'index : on va pouvoir afficher la date du jour et l'heure via un bouton

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<DOCTYPE html>
-	<head>
-	</head>
-	<body>
-		<div id="container">
-			bienvenue sur ce nouveau projet<br>
-			pour l'instant cette page ne sert pas à grand chose
-		</div>
-	</body>
+!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Site de démo pour le cours Git d'OC</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link href="style.css" rel="stylesheet">
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="container">
+          <p>ce bouton permet de réaliser un non-appel ajax à l'horloge parlante : <button id="btn"></button></p>
+        <div id="heure"><p>nous sommes le <?=date("d/m/Y")?> au moment du chargement de la page il était <?=date("h\hi")?></p></div>
+    </div>
+</body>
 </html>
