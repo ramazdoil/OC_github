c03a1526641d95fa9099c41023849ef8425b85ad
fflesch <fflesch.dev@gmail.com>
2015-08-22 17:30:26 +0200

    ajout d'un index quasi vide

---
+++ index.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+	<head>
+	</head>
+	<body>
+		<div id="container">
+			bienvenue sur ce nouveau projet<br>
+			pour l'instant cette page ne sert pas à grand chose
+		</div>
+	</body>
+</html>
